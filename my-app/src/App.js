eca07b5af1e8588483ba6ea4940758b78f2451bd
IlyaKash <iliakashkarov@gmail.com>
2024-02-29 20:13:53 +0700

    added lab1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './components/button'
 import Container from './components/container';
 function App() {
+  const [lab, setLab]=useState(0);
 
   const handleClick1 = () => {
     alert('Button 1 clicked!');
@@ -15,17 +17,71 @@ function App() {
     window.location.href = 'https://github.com/IlyaKash/web_lab';
   };
 
+  function change_lab(num){
+    setLab(num)
+  }
+
+  const  handleClick_lab2= ()=>{
+    return (
+      <Container>
+        <h1>Лабараторная №2</h1>
+        <Button onClick={handleClick1}>Alert</Button>
+        <Button onClick={handleClick2}>Console</Button>
+        <Button onClick={handleClick3}>GitGub</Button>
+      </Container>
+    )
+  }
+
+  const RenderList=props =>{
+    const myArray=[
+      {
+        name: "lab1",
+        number: 1,
+      },
+      {
+        name: "lab2",
+        number: 2,
+      },
+      {
+        name: "lab3",
+        number: 3,
+      },
+      {
+        name: "lab4",
+        number: 4,
+      },
+      {
+        name: "lab5",
+        number: 5,
+      },
+    ];
+
+    return (
+      <Container className="ContainerLeftMenu">
+        <ul>
+          {myArray.map(item=> (
+            <li key={item.number}><Button onClick={() => change_lab(item.number)}>{item.name}</Button></li>
+          ))}
+        </ul>
+      </Container>
+    )
+  }
+
   return (
     <div className="App">
-      <header>
+      <header className="Head">
+        <h1>Отчет по лабораторным, Кашкаров И.И. 4.105-1</h1>
       </header>
       <main>
-        <Container>
-          <h1>Кашкаров И.И. 4.105-1</h1>
-          <Button onClick={handleClick1}>button_1</Button>
-          <Button onClick={handleClick2}>button_2</Button>
-          <Button onClick={handleClick3}>button_3</Button>
-        </Container>
+        <div className="Left_menu">
+          {RenderList()}
+        </div>
+        <div className="Content">
+          {lab===1 && (
+            <iframe src='/lab1/lab1.html' title='Lab1' width="80%" height="500px" />
+          )}
+          {lab===2 && handleClick_lab2()}
+        </div>
       </main>
     </div>
   );
--- my-app/src/components/container.js
+++ my-app/src/components/container.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Container = ({ children }) => {
+const Container = ({ children, className }) => {
   return (
-    <div style={{ border: '1px solid black', padding: '20px', margin: '20px' }}>
+    <div className={className} style={{ border: '1px solid black', padding: '20px', margin: '20px' }}>
       {children}
     </div>
   );
